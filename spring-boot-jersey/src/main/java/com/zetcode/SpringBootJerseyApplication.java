77259822484f7e4261a64aacb8dcbad0224d66c4
Bryan Job <bryan.wan.job@gmail.com>
2021-06-29 15:46:16 -0500

    LEARN-3: check in code created by Spring Initializr https://start.spring.io/

---
+++ spring-boot-jersey/src/main/java/com/zetcode/SpringBootJerseyApplication.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyApplication.class, args);
+	}
+
+}
